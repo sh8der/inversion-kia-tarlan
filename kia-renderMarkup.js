438e7fd637328cbd761e587ea2dfd04cbf7e58fc
sh8der <youxenux@gmail.com>
2021-07-27 19:05:20 +0600

    chore: добавить файл скрипта для генерации разметки слайдов на стороне референсого сайта kia.ru

---
+++ kia-renderMarkup.js
@@ -0,0 +1,29 @@
+var automate = {
+  titles: [],
+  videos: [],
+  slideTemplate: `[section_inner padding="0px" class="no-padding"]
+
+[ux_banner height="210px" bg="3376" bg_overlay="rgba(5, 20, 31, 0.2)" bg_pos="55% 52%" class="mr-half"]
+
+[text_box position_x="50" position_y="50"]
+
+[video_button video="{{video}}"]
+
+[/text_box]
+
+[/ux_banner]
+[gap height="10px"]
+
+<h5 style="text-align: left;">{{title}}</h5>
+
+[/section_inner]`,
+  setTitles: function() { document.querySelectorAll('.news-card-content__title').forEach( title => this.titles.push(title.innerText) )},
+  setVideos: function() { document.querySelectorAll('.videos-gallery__iframe-body iframe').forEach( iframe => this.videos.push('https://www.youtube.com/watch?v=' + iframe.src.split('/embed/')[1].split('?')[0]) )},
+  getMarkup: function() {
+    var markup = '';
+    this.titles.forEach((title, index) => {
+      markup = markup.concat(this.slideTemplate.replace('{{video}}', this.videos[index]).replace('{{title}}', title))
+    })
+    console.log(markup)
+  }
+}
