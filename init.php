d3100f022fafa76da59e60df2011afc99ce9a987
sh8der <youxenux@gmail.com>
2021-07-27 19:03:41 +0600

    feat: добавить отступ у слайдов слайдера с классом .model-video-gallery

--- init.php
+++ init.php
@@ -9,5 +9,5 @@ require_once __DIR__ . '/ux_builder/override_follow_shortcode.php';
 
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_script('sh8der-custom-j', get_stylesheet_directory_uri() . '/sh8der/custom.js?v6', array('jquery'));
-  wp_enqueue_style('sh8der-custom-s', get_stylesheet_directory_uri() . '/sh8der/custom.css?v15', array('flatsome-main'));
+  wp_enqueue_style('sh8der-custom-s', get_stylesheet_directory_uri() . '/sh8der/custom.css?v17', array('flatsome-main'));
 });
